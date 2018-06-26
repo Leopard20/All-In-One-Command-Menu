ae5b9dd7a81e34fe720c5c6e48b5313b8a1f4b06
Leo <leopard20@users.noreply.github.com>
2018-06-27 02:05:23 +0430

    Added mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "All-In-One AI Command Menu";
+author = "Leopard20";
+description = "All-In-One AI Command Menu by Leopard20 and WindWalking";
+overview = "All-In-One AI Command Menu by Leopard20 and WindWalking";
+overviewText = "All-In-One AI Command Menu by Leopard20 and WindWalking";
+overviewFootnote = "";
